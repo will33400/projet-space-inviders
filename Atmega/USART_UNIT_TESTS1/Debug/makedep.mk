468d8e50d1973f8c5cbe8f65a755ccfac4b73642
will33400 <lucasadam33400@gmail.com>
2020-01-13 16:23:15 +0100

    update files

---
+++ Atmega/USART_UNIT_TESTS1/Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+src\ASF\common\services\clock\mega\sysclk.c
+
+src\ASF\common\services\serial\usart_serial.c
+
+src\ASF\common\utils\stdio\read.c
+
+src\ASF\common\utils\stdio\write.c
+
+src\ASF\common\utils\unit_test\suite.c
+
+src\ASF\mega\boards\stk600\rc128x_rfx\init.c
+
+src\ASF\mega\drivers\usart\usart_megarf.c
+
+src\unit_tests.c
+
